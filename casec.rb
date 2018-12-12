5bcd0283d973683a7638a4f18261f3f0dbf8d01d
mingrammer <mingrammer@gmail.com>
2018-12-13 01:58:25 +0900

    Brew formula update for casec version v0.1.2

--- casec.rb
+++ casec.rb
@@ -1,9 +1,9 @@
 class Casec < Formula
   desc "A text case converter"
   homepage "https://github.com/mingrammer/casec"
-  url "https://github.com/mingrammer/casec/releases/download/v0.1.1/casec_0.1.1_darwin_amd64.tar.gz"
-  version "0.1.1"
-  sha256 "f7590c79675230285b0dc1a13891e6fa60aea0c3c071ead98dafe45028c2ad61"
+  url "https://github.com/mingrammer/casec/releases/download/v0.1.2/casec_0.1.2_darwin_amd64.tar.gz"
+  version "0.1.2"
+  sha256 "b2e31cd821e45314b647b4ea304160d61d0039e5e73a949390d83db25e47ce29"
 
   def install
     bin.install "casec"
